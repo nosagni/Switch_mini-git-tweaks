a211a3e850e92165a50f27a50df34e0e87006097
dannephoto <dudek53@gmail.com>
2020-09-11 21:42:07 +0200

    colors

--- Switch_mini.app/Contents/Switch_mini_app.sh
+++ Switch_mini.app/Contents/Switch_mini_app.sh
@@ -842,7 +842,7 @@ while true; do
 
     cat <<EOF
     -----------
-    $(tput bold)Switch mini$(tput sgr0)
+    $(tput bold)$(tput setaf 3)Switch mini$(tput sgr0)
     -----------
 
     $(tput bold)MLV input: $(tput setaf 4)$in$(tput sgr0)
@@ -872,8 +872,8 @@ while true; do
     $(tput bold)(20) bad pixel method: $(tput sgr0)(mlvfs=0),(raw2dng=1),default=1$(tput bold)$(tput setaf 4) $bpm$(tput sgr0)
     $(tput bold)(21) skip loading .IDX (XREF). Use if audio is problematic$(tput sgr0) $xref$(tput sgr0)
     $(tput bold)(22) apply auto white balance to your dng files $(tput sgr0) $awb$(tput sgr0)
-    $(tput bold)(23) prores output$(tput sgr0)(ffmpeg) $prores$(tput sgr0)
-    $(tput bold)(24) lowres proxy output$(tput sgr0)(ffmpeg) $proxy$(tput sgr0)
+    $(tput bold)$(tput setaf 6)(23) prores output$(tput sgr0)(ffmpeg) $prores$(tput sgr0)
+    $(tput bold)$(tput setaf 6)(24) lowres proxy output$(tput sgr0)(ffmpeg) $proxy$(tput sgr0)
 
     $(tput bold)$(tput setaf 4)(h)  HOWTO$(tput sgr0)
     $(tput bold)$(tput setaf 1)(R)  reset switches$(tput sgr0)
